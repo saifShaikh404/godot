bf30d19fc5b13cf29a9a205b7cc8aa5837c6cfe4
saifShaikh404 <shaikhsaif78673@gmail.com>
2024-04-29 00:37:42 +0530

    Area2D and Signal
    Area2D :-  3rd Physics node give signals when certain thing or actions happened in that area
    
    Timer :- timer node for set timer based coding

---
+++ scenes/level/level.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_area_body_entered(_body):
+	print('Player Entered')
+
+
+func _on_area_body_exited(_body):
+	print('Player Exited')
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,27 @@
 extends CharacterBody2D
 
+var can_laser: bool = true;
+var can_grenade: bool = true;
+
 func _process(_delta):
-	var direction = Input.get_vector("left","right","up","bottom")
-	velocity = direction * 200
-	move_and_slide()
+	var direction = Input.get_vector("left","right","up","bottom");
+	velocity = direction * 400;
+	move_and_slide();
+	
+	if Input.is_action_just_pressed("fire") && can_laser:
+		print('Fire laser');
+		can_laser = false;
+		$LaserTimer.start();
+		
+	if Input.is_action_just_pressed("throw") && can_grenade:
+		print('Throw grenade');
+		can_grenade = false;
+		$GrenadeTimer.start();
+
+
+func _on_laser_timer_timeout():
+	can_laser = true; # Replace with function body.
+
+
+func _on_grenade_timer_timeout():
+	can_grenade = true; # Replace with function body.
